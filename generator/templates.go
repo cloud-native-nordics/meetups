ae3d4ea790f1f32914c6d3cc119a0a6d160d2e96
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2019-08-07 09:54:09 +0300

    Add more community information

--- generator/templates.go
+++ generator/templates.go
@@ -45,8 +45,30 @@ Repository to gather all meetup information and slides from Cloud Native Nordic 
 {{end}}
 ## Join our Community!
 
+### Slack
+
 To facilitate and help each other in between meetups and different geographical locations, we have set up a joined Slack Community.
 
 In order to sign-up, go to [www.cloudnativenordics.com](https://www.cloudnativenordics.com) and enter your e-mail. Shortly hereafter you will receive an email with instructions to join the community.
+
+### Mailing List
+
+In order to share documents and calendar invites across our community, we have set up a Mailing List using Google Groups.
+
+Please join our group at [#cloud-native-nordics](https://groups.google.com/forum/#!forum/cloud-native-nordics)!
+
+### Speaking Opportunities
+
+If you'd like to speak at a meetup, please join our [#cloud-native-nordics-speakers](https://groups.google.com/forum/#!forum/cloud-native-nordics-speakers) Mailing List. In this low-traffic group you can get information about speaking opportunities
+across all of the Nordic countries!
+
+### Monthly Calls
+
+We're organizing public monthly community calls where everybody is invited to join.
+The calls are recorded and will be uploaded to YouTube afterwards.
+
+The **[meeting agenda](https://docs.google.com/document/d/1JxAZcNrGrK89-ErVOKku7Ik76ccSXPa66S8zdbVDr2g/edit#heading=h.cdvsk7jju5f9)** 
+is publicly available, please join the [#cloud-native-nordics](https://groups.google.com/forum/#!forum/cloud-native-nordics) mailing
+list to get write-access.
 `
 )
